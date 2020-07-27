c20827786fe63081df866798960235ca7e97e35c
Mark Jennings <mrj.masc@gmail.com>
2020-07-26 20:53:17 -0400

    standard library

--- src/c/cortex-m3/capstone/main.c
+++ src/c/cortex-m3/capstone/main.c
@@ -1,4 +1,11 @@
-#include "main.h"
+#include <assert.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+uint32_t a = 0;
+
+void initialize();
 
 int main(void)
 {
@@ -12,62 +19,7 @@ int main(void)
 
 void initialize()
 {
-	configureSystemClock();
-	delays_init(SYS_CLK);
-	HCSR04();
+	printf("Initializing...");
 }
 
-void configureSystemClock(void)
-{	
-	//Enable HSI
-	enableHSI();
-	
-	//Set HSE ON 
-	enableHSE();
-	
-	setPLLMULL(RCC_CFGR_PLLMULx9);
-	setUSBPRE(USB_PLL_DIVx1);
-	setPLLSRC();
-	setAPB1_PSC(RCC_CFGR_HCLK_DIVx2);	
-	
-	//Wait for HSE ready
-	while(!isHSERDY());
-	
-	//Turn on PLL
-	enablePLL();
-		
-	//Wait for PLL ready
-	while(!isPLLRDY());
-	
-	//Set flash wait states to 2
-	setPRFTB(FLASH_PRFTB_ENABLE);
-	setWaitStates(FLASH_WAIT_STATES_TWO);
-	
-	//Switch system clock to PLL
-	setSW(RCC_CFGR_SW_PLL);
-	
-	//Wait for switch to PLL
-	while(getSWS() != RCC_CFGR_SW_PLL);
-	
-	//Turn off HSI to save power
-	disableHSI();
-}	
-
-void HCSR04(void)
-{	
-	enablePeripheralClock(RCC_APB2ENR_IOPAEN);
-	enablePeripheralClock(RCC_APB2ENR_IOPBEN);
-	
-	configurePortPin(PORTB, GPIO_PIN1, CNF_MODE_OUTPUT_GP_PP, MODE_OUTPUT_2MHz);	//TRIG
-	configurePortPin(PORTA, GPIO_PIN8, CNF_MODE_INPUT_FLOAT, MODE_INPUT);		//ECHO
-	
-	pinState(PORTB, GPIO_PIN1, LOW);
-}
-
-void trigger(void)
-{
-	pinState(PORTB, GPIO_PIN1, HIGH); 
-	delay_ms(1);
-	pinState(PORTB, GPIO_PIN1, LOW);	
-}
 
---
+++ src/c/cortex-m3/project/main.c
@@ -0,0 +1,73 @@
+#include "main.h"
+
+int main(void)
+{
+	initialize();
+	
+	while(1)
+	{
+
+	}
+}
+
+void initialize()
+{
+	configureSystemClock();
+	delays_init(SYS_CLK);
+	HCSR04();
+}
+
+void configureSystemClock(void)
+{	
+	//Enable HSI
+	enableHSI();
+	
+	//Set HSE ON 
+	enableHSE();
+	
+	setPLLMULL(RCC_CFGR_PLLMULx9);
+	setUSBPRE(USB_PLL_DIVx1);
+	setPLLSRC();
+	setAPB1_PSC(RCC_CFGR_HCLK_DIVx2);	
+	
+	//Wait for HSE ready
+	while(!isHSERDY());
+	
+	//Turn on PLL
+	enablePLL();
+		
+	//Wait for PLL ready
+	while(!isPLLRDY());
+	
+	//Set flash wait states to 2
+	setPRFTB(FLASH_PRFTB_ENABLE);
+	setWaitStates(FLASH_WAIT_STATES_TWO);
+	
+	//Switch system clock to PLL
+	setSW(RCC_CFGR_SW_PLL);
+	
+	//Wait for switch to PLL
+	while(getSWS() != RCC_CFGR_SW_PLL);
+	
+	//Turn off HSI to save power
+	disableHSI();
+}	
+
+void HCSR04(void)
+{	
+	enablePeripheralClock(RCC_APB2ENR_IOPAEN);
+	enablePeripheralClock(RCC_APB2ENR_IOPBEN);
+	
+	configurePortPin(PORTB, GPIO_PIN1, CNF_MODE_OUTPUT_GP_PP, MODE_OUTPUT_2MHz);	//TRIG
+	configurePortPin(PORTA, GPIO_PIN8, CNF_MODE_INPUT_FLOAT, MODE_INPUT);		//ECHO
+	
+	pinState(PORTB, GPIO_PIN1, LOW);
+}
+
+void trigger(void)
+{
+	pinState(PORTB, GPIO_PIN1, HIGH); 
+	delay_ms(1);
+	pinState(PORTB, GPIO_PIN1, LOW);	
+}
+
